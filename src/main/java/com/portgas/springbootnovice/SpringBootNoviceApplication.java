b0f179c48bc72159820a62df4728ed0b56802ec7
tuan.nguyen@the-hash.com <Tachanminami1994>
2020-04-24 23:28:47 +0700

    First commit project

---
+++ src/main/java/com/portgas/springbootnovice/SpringBootNoviceApplication.java
@@ -0,0 +1,13 @@
+package com.portgas.springbootnovice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNoviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portgas/springbootnovice/SpringBootNoviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portgas.springbootnovice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNoviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
