891ae3840bdae21541d8746534b9f5a901d06c76
tuan.nguyen@the-hash.com <Tachanminami1994>
2020-04-26 10:00:31 +0700

    config logger

--- src/main/java/com/portgas/springbootnovice/service/core/RoleServiceImpl.java
+++ src/main/java/com/portgas/springbootnovice/service/core/RoleServiceImpl.java
@@ -4,6 +4,8 @@ import com.portgas.springbootnovice.config.database.DBContextHolder;
 import com.portgas.springbootnovice.config.database.DBType;
 import com.portgas.springbootnovice.model.role.Role;
 import com.portgas.springbootnovice.model.role.query.QRole;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private static final Logger LOGGER= LoggerFactory.getLogger(RoleServiceImpl.class);
 
     @Transactional(readOnly = true)
     @Override
@@ -21,9 +24,9 @@ public class RoleServiceImpl implements RoleService {
 
         List<Role> roleList = new QRole()
                 .findList();
-        System.out.println("---------------- "+ dbType.name() +" -------------");
+        LOGGER.info("---------------- "+ dbType.name() +" -------------");
         for (Role r:roleList){
-            System.out.println("Name : " + r.getName());
+            LOGGER.info("Name : " + r.getName());
         }
 
         DBContextHolder.clearDbType();
